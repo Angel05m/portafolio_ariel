88efaf719cf0f44d9683325ac389ba59dd4c478c
Angel05m <aasm052002@gmail.com>
2025-12-09 00:31:23 -0700

    add: componente menu y perfil

---
+++ src/components/menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export function Menu() {
+  return (
+    <>
+      <nav className='text-3xl bg-gray-500 p-4'>
+        Hola soy el menu
+      </nav>
+    </>
+  );
+}
